7d5a0d983ce9264b1d2e0ac76690631997519bc9
Adrian Lin <adrianylin@gmail.com>
2015-10-26 10:59:03 -0700

    course_data insert stub

---
+++ utilities/course_data_insert_stub.sql
@@ -0,0 +1,10 @@
+INSERT INTO course_data
+(id, title, short_desc, long_desc, course_link,
+video_link, start_date, course_length, course_image, category,
+site, course_fee, language, certificate, university,
+time_scraped)
+VALUES
+(DEFAULT, 'a', 'a', 'a', 'a',
+'a', '2015-10-1', 1, 'a', 'a', 
+'a', 1, 'a', 'yes', 'a',
+'2015-10-1')
